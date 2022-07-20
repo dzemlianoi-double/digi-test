49257855981babc5c6a20b8de6eb0d7d50ace701
DenysZemlianoi <denis.zemlianoy@gmail.com>
2022-07-20 18:35:51 +0300

    DIG-5: Add markup for sign in and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for(:users, only: %w[sessions#new session#destroy])
+  devise_for(:users, only: %i[sessions registrations])
 
   root to: redirect('users/sign_in')
 end
